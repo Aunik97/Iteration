466fbe6b168e3bddd57aef3d85cc37848b482e30
Aunik Hussain <31704@longroad.ac.uk>
2014-10-20 12:16:47 +0100

    commit

---
+++ task 1 improving a piece of code.py
@@ -0,0 +1,10 @@
+total = 0
+
+for count in range(1,9):
+    number = int(input("Please enter number"))
+    total =  number + total
+print("The total of these numbers is {0}.".format(total))
+
+    
+
+
